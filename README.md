849229df4a5ba4bd017a7e09791fd0e2f4edd681
Damish <damish.shah@gmail.com>
2020-01-18 19:53:51 -0800

    Add ability to fullscreen video and drag local preview around screen

--- README.md
+++ README.md
@@ -4,10 +4,10 @@ Simple WebRTC video conferencing application
 ### Things to do
 
 1. Allow user to move and resize their local preview window
-2. Allow fullscreen of videos
 
 ### Done
 
 1. Make chat collapsible
 2. Add multiple peer connections
 3. Gracefully handle callers leaving
+4. Allow fullscreen of videos
--- js/main.js
+++ js/main.js
@@ -351,8 +351,75 @@ function randomToken() {
 
 // --- Window functions ---
 
+var videosElement = document.querySelector("#videos");
+videosElement.onkeydown = videosElementOnKeyDownHandler;
+videosElement.ondblclick = toggleFullscreen;
+
+function videosElementOnKeyDownHandler(event) {
+  console.debug("Received key: " + event.keyCode);
+  switch(event.keyCode){
+    case 70: // KeyCode for 'f'
+      toggleFullscreen();
+      break;
+ }
+}
+
+function toggleFullscreen() {
+  if (!document.fullscreenElement) {
+    videosElement.requestFullscreen().catch(err => {
+      alert(`Error attempting to enable full-screen mode: ${err.message} (${err.name})`);
+    });
+  } else {
+    document.exitFullscreen();
+  }
+}
+
 window.onbeforeunload = function() {
   sendMessageToRoom('bye');
+}
+
+// Shamelessly taken from https://www.w3schools.com/howto/howto_js_draggable.asp:
+dragElement(localVideo);
+
+function dragElement(elmnt) {
+  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+  if (document.getElementById(elmnt.id + "header")) {
+    // if present, the header is where you move the DIV from:
+    document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
+  } else {
+    // otherwise, move the DIV from anywhere inside the DIV:
+    elmnt.onmousedown = dragMouseDown;
+  }
+
+  function dragMouseDown(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // get the mouse cursor position at startup:
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    document.onmouseup = closeDragElement;
+    // call a function whenever the cursor moves:
+    document.onmousemove = elementDrag;
+  }
+
+  function elementDrag(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // calculate the new cursor position:
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    // set the element's new position:
+    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+  }
+
+  function closeDragElement() {
+    // stop moving when mouse button is released:
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
 }
 
 // TODO: Configure and add a TURN server
